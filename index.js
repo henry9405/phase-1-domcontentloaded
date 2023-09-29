97de0944f26e5016911197ae282eec30d3cd86ba
henry9405 <144359211+henry9405@users.noreply.github.com>
2023-09-29 17:46:20 +0300

    textcontent

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+// index.js
+
+// Define a function to change the text content of the <p> element
+function changeText() {
+    const pElement = document.querySelector('p');
+    pElement.textContent = 'This is really cool!';
+}
+
+// Add an event listener for the 'DOMContentLoaded' event
+document.addEventListener('DOMContentLoaded', changeText);
